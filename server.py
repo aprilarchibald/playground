4984bdab4e59877014bc9e1ebbba8ecbf867cc93
April Archibald <aprilsclearskies@gmail.com>
2022-04-08 11:52:16 -0700

    working

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 
 from flask import Flask, render_template
 app=Flask(__name__)
-
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", num =3,color ="blue")
 
 @app.route('/play/<int:num>')
 def play(num):
-    return render_template("index_html", num=num, color ="blue")
+    return render_template("index.html", num=num, color ="blue")
 
 @app.route('/play/<int:num>/<color>')
 def play2(num, color):
-    return render_template("index_html", num = num, color =color)
+    return render_template("index.html", num = num, color =color)
 
 
 if __name__=="__main__":
